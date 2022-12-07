f81cd3f08233e305f5a55b8c411cff3977d5dda0
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-12-08 00:09:10 +0600

    modify premium acce page

--- src/pages/PremiumAccess/PremiumAccess.js
+++ src/pages/PremiumAccess/PremiumAccess.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const PremiumAccess = () => {
     return (
-        <div>
-            WellCome to Premium Access!!!
+        <div className=' mx-auto text-center'>
+            <h1 className='mt-5'>Congratulation for Premium Access!!!</h1>
+            <img className='img-fluid' src="https://media.istockphoto.com/id/1136522116/vector/lettering-for-card.jpg?s=612x612&w=0&k=20&c=gH9eHw4oY24YMVrsEobhEI1mztiBJ8s05xdN7D57M-Q=" alt="" />
         </div>
     );
 };
