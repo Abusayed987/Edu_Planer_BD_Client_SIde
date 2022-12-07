d1de88edaa5cc59d933a8693915d1c20cfb2d674
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-12-07 15:19:11 +0600

    added course details download option

--- src/pages/CourseDetails/CourseDetails.js
+++ src/pages/CourseDetails/CourseDetails.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import Pdf from "react-to-pdf";
 import { Col, Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link, useLoaderData } from 'react-router-dom';
-
+import { FaDownload } from 'react-icons/fa';
+const ref = React.createRef();
 const CourseDetails = () => {
     const courseDetail = useLoaderData()
     const { picture, course_description, course_title, price, teacher_name } = courseDetail
@@ -12,29 +15,43 @@ const CourseDetails = () => {
 
     return (
         <Card className='w-50 mx-auto mt-4 text-center'>
-            <Card.Header>
-                <h2 className='text-primary'>{course_title}</h2>
-            </Card.Header>
-            <Card.Img variant="top" src={picture} />
-            <Card.Body>
-                <Card.Title className='d-flex justify-content-between'>
-                    <div>
-                        Instructor: {teacher_name}
+
+            <div ref={ref}>
+                <Card.Header className='d-flex justify-content-between'>
+                    <h2 className='text-primary ' >
+                        {course_title}
+                    </h2>
+                    <Pdf targetRef={ref} filename={`${course_title}.pdf`}>
+                        {({ toPdf }) => <Button
+                            className=''
+                            variant="outline-info"
+                            onClick={toPdf}>
+                            Download <FaDownload></FaDownload>
+                        </Button>}
+                    </Pdf>
+                </Card.Header>
+                <Card.Img variant="top" src={picture} />
+                <Card.Body>
+                    <Card.Title className='d-flex justify-content-between'>
+                        <div>
+                            Instructor: {teacher_name}
+                        </div>
+                        <div className='text-success'>
+                            ৳{price}90
+                        </div>
+                    </Card.Title>
+                    <Card.Text>
+                        <div className='border border-rounded p-3 mt-3'>
+                            {course_description}
+                        </div>
+                    </Card.Text>
+                    <div className='d-flex justify-content-between'>
+                        <Link className='btn btn btn-outline-success  mx-auto mb-3' to='/courses'>Back to Courses</Link>
+                        <Link className='btn btn btn-primary  mx-auto mb-3' >Get Premium Access </Link>
                     </div>
-                    <div className='text-success'>
-                        ৳{price}90
-                    </div>
-                </Card.Title>
-                <Card.Text>
-                    <div className='border border-rounded p-3 mt-3'>
-                        {course_description}
-                    </div>
-                </Card.Text>
-                <div className='d-flex justify-content-between'>
-                    <Link className='btn btn btn-outline-success  mx-auto mb-3' to='/courses'>Back to Courses</Link>
-                    <Link className='btn btn btn-primary  mx-auto mb-3' >Get Premium Access </Link>
-                </div>
-            </Card.Body>
+                </Card.Body>
+            </div>
+
         </Card>
     );
 };
